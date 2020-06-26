d4ada05e5128823f74e7e659eb35c6a9d55c2678
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-06-26 19:33:00 -0300

    Update index.m

--- index.m
+++ index.m
@@ -1,13 +1,9 @@
-function out = index(P,deltax,deltaz)
-%  Função que calcula os índices das células nas direções X e Z
+function out = index(P,cellLengthInXDirection,cellLengthInZDirection)
+  %  Função que calcula os índices das células nas direções X e Z
+  %  P= É um vector de duas componentes. P é a soma da componente dos ultimos 2 pontos, divido por 2
 
-%INPUT's:
-%  P= É um vector de duas componentes. P é a soma da componente dos ultimos 2 pontos, divido por 2
-% deltax= comprimento da célula na direção X
-% deltaz= comprimento da célula na direção Z
+  rowIndex = (fix(P(2)/cellLengthInZDirection)+1);
+  collumnIndex = (fix(P(1)/cellLengthInXDirection)+1);
 
-i1 = (fix(P(2)/deltaz)+1);
-i2 = (fix(P(1)/deltax)+1);
-
-out=[i1 i2]; % Saídas i1 e i2 representam respectivamente linha e coluna do ponto em questão
+  out=[rowIndex collumnIndex];
 end
