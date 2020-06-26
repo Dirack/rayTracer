e84dd1767e7ebe1e38f91dfb348a7f0988f7a8e8
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-06-26 19:49:51 -0300

    Update index.m

--- index.m
+++ index.m
@@ -1,8 +1,8 @@
 function calculateCellIndexInXAndZDirections = index(P,cellLengthInXDirection,cellLengthInZDirection)
   %  P= É um vector de duas componentes. P é a soma da componente dos ultimos 2 pontos, divido por 2
 
-  indexInXDirection = (fix(P(2)/cellLengthInZDirection)+1);
-  indexInZDirection = (fix(P(1)/cellLengthInXDirection)+1);
+  indexInZDirection = (fix(P(2)/cellLengthInZDirection)+1);
+  indexInXDirection = (fix(P(1)/cellLengthInXDirection)+1);
 
   calculateCellIndexInXAndZDirections=[indexInZDirection indexInXDirection];
 end
