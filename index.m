fb6ffac61de07d28a08a86d88b21495417c8ee34
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-06-26 19:42:36 -0300

    Update index.m

--- index.m
+++ index.m
@@ -1,9 +1,8 @@
-function out = index(P,cellLengthInXDirection,cellLengthInZDirection)
-  %  Função que calcula os índices das células nas direções X e Z
+function calculateCellIndexInXAndZDirections = index(P,cellLengthInXDirection,cellLengthInZDirection)
   %  P= É um vector de duas componentes. P é a soma da componente dos ultimos 2 pontos, divido por 2
 
-  rowIndex = (fix(P(2)/cellLengthInZDirection)+1);
-  collumnIndex = (fix(P(1)/cellLengthInXDirection)+1);
+  indexInXDirection = (fix(P(2)/cellLengthInZDirection)+1);
+  indexInZDirection = (fix(P(1)/cellLengthInXDirection)+1);
 
-  out=[rowIndex collumnIndex];
+  calculateCellIndexInXAndZDirections=[indexInZDirection indexInXDirection];
 end
